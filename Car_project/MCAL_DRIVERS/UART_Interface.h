5fd9b294c2c72cc39ee808843197dad6d63742d0
Saber Mahmoud <134223747+SaberMahmoud1@users.noreply.github.com>
2024-11-29 23:22:03 +0200

    Add files via upload

---
+++ Car_project/MCAL_DRIVERS/UART_Interface.h
@@ -0,0 +1,21 @@
+/*
+ * UART_Interface.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: HP
+ */
+
+#ifndef UART_INTERFACE_H_
+#define UART_INTERFACE_H_
+
+/*     Libraries    */
+#include "../Serivecs_driver/STD_TYPES.h"
+#include "../Serivecs_driver/BIT_MATH.h"
+
+void UART_VidInit () ;
+void UART_VidSendData(u8 Copy_U8Data);
+u8 UART_VidRecieveData();
+void UART_VidSendString (u8 Copy_U8String[]);
+void SetCallBack(  void (*ptr)(void) );
+
+#endif /* UART_INTERFACE_H_ */
